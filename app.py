8e3363ece287ec3e962a2c2638c239b53936679b
Arihant Pal <69034460+Ari1009@users.noreply.github.com>
2023-04-08 04:42:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 import streamlit as st
+import io
+import numpy as np
+from PIL import Image
 import tensorflow as tf
-import cv2
-
-import time
-
-
-from utils import *
+import efficientnet.tfkeras as efn
 
 st.set_page_config(page_title='Virtue', page_icon = 'assets/images/logo.png')
+# Title and Description
 st.title("Virtue Image")
+st.write("Just Upload your Plant's Leaf Image and get predictions if the plant is healthy or not") 
 
 
 
-method = st.selectbox('Capture or Upload an Image', ('Upload Image', 'Capture Image'))
 
-if method == 'Upload Image':
-    image_file = st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"])
-else:
-    image_file = st.camera_input("Capture Image")
+model = tf.keras.models.load_model('model.h5')
 
 
+uploaded_file = st.file_uploader('Choose your image', type=['png', 'jpg'])
 
+predictions_map = {0:'is healthy', 1:'hast Multiple Diseases', 2:'has rust', 3:'has scab'}
+
+if uploaded_file is not None:
+    image = Image.open(io.BytesIO(uploaded_file.read()))
+    st.image(image, use_column_width=True)
+    
+    resized_image = np.array(image.resize((512,512)))/255. # Resize image and divide pixel number by 255. for having values between 0 and 1 (normalize it)
+    
+    image_batch = resized_image[np.newaxis, :, :, :]
+    
+    predictions_arr = model.predict(image_batch)
+    
+    predictions = np.argmax(predictions_arr)
+
+    result_text = f'The plant leaf {predictions_map[predictions]}'
+
+    if predictions == 0:
+        st.success(result_text)
+    else:
+        st.error(result_text)
+
+
+
+
+
+    
